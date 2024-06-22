930f3032c0bdc0f0e0ff9cefdbc6ce3e13236ee3
Parthiv Padariya <parthiv.padariya@gmail.com>
2024-06-22 16:27:07 +0530

    init project

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+// ws, express, 
+import { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({port:8080});
+
+wss.on('connection', (ws) => {
+    ws.on('error', console.error);
+
+    ws.on('message', (data) => {
+        console.log('received %s', data);
+    })
+
+    ws.send('something')
+})
